030199ccf5cf6630d30a39fb9b990c04f31fa7a4
Blervin <126615593+Blervin1@users.noreply.github.com>
2025-01-02 23:00:27 +0100

    fix  insertAdjacentHTML.js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,11 +6,8 @@ for (const input of inputs) {
   const labelText = input.name
     .replace(/([A-Z])/g, ' $1')
     .replace(/^./, (str) => str.toUpperCase());
-
-  input.insertAdjacentHTML(
-    'afterend',
-    `<label class="field-label" for="${input.id}">${labelText}</label>`,
-  );
-
+  
+  input.insertAdjacentHTML('afterend', `<label class="field-label" for="${input.id}">${labelText}</label>`);
+  
   input.placeholder = labelText;
 }
